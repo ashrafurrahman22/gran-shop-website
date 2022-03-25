6b4551b80bd87f8f1f2a6b7e4d157694e5c8eea9
ashrafur22 <ashrafurrahman22@gmail.com>
2022-03-25 23:34:07 +0600

    second commit

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
+    const [products, setProducts] = useState([])
+    useEffect( () => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    } , [])
     return (
         <div>
             <h2>Products compo</h2>
