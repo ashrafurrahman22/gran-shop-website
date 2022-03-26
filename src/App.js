6a029500e500c9a3f58461737a64eb2173a1ad64
ashrafur22 <ashrafurrahman22@gmail.com>
2022-03-26 12:35:43 +0600

    ninth

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Products from './Components/Products/Products';
 import Header from './Components/Header/Header';
+import Question from './Components/Question-answer/Question';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Products></Products>
+      <Question></Question>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,16 +3,32 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='container'>
-            <div className="shop-name">
-                <h3>Gran Shop</h3>
-            </div>
-        <div className="navLink">
-            <a href="/home">Home</a>
-            <a href="/products">Products</a>
-            <a href="/location">Location</a>
-            <a href="/contact">Contact</a>
-        </div>
+        <div classNameName='container'>
+ <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid bg-info p-3">
+    <a className="navbar-brand" href="#">Gran Shop</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Products</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Location</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
         </div>
     );
 };
