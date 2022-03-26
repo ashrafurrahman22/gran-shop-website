38f16d5b08085e6a1fe5dab03286ca59bf6aaffe
ashrafur22 <ashrafurrahman22@gmail.com>
2022-03-26 09:36:36 +0600

    sixth

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
 
 const Product = ({product}) => {
-    const {name, price, img} = product
+    const {name, price, img, ml} = product
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className="specification">
             <h3>{name}</h3>
-            <h4>Price: {price}</h4>
-            <button>Add To Cart</button>
+            <h4>Ml: {ml}</h4>
+            <h3>Price: {price}</h3>
+            </div>
+            <button className='cartBtn'>Add To Cart<FontAwesomeIcon icon={faCartShopping} />  </button>
         </div>
     );
 };
